ef76545434cdb07efc54804efe7049b3163bc8c7
vijay <vijayaraghavan16315@cse.ssn.edu.in>
2021-12-21 08:26:56 +0530

    Update

--- contents.js
+++ contents.js
@@ -1,1 +1,12 @@
 //Understand the core principles
+// Before react:
+//Basic Js, HTML, Css
+// Just simple forms
+// React key concepts
+// Imperative
+// Declarative -  React uses this // just declare - I'll do the DOM manipulation
+// React component architecture
+// One way data flow -> State changes -> update the components according to the new state
+// UI library
+
+// React basics -  14
